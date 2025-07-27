7392cbeb83bb0b02526b70bfe61f439c55e0ebbe
ThomasOudhoff <thomasoudhoff@live.nl>
2025-07-27 17:34:12 +0200

    Eerste commit, project aangemaakt

---
+++ src/main/java/com/thomas/medicatieinnameApp/MedicatieinnameAppApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.medicatieinnameApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicatieinnameAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicatieinnameAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomas/medicatieinnameApp/MedicatieinnameAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomas.medicatieinnameApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicatieinnameAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
